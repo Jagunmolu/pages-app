d62f6f70ab66ed058cda21ca58a662b92ea85479
Jagunmolu <kolawolejonathan@gmail.com>
2022-04-19 23:25:37 +0100

    Initial Commit

---
+++ pages/urls.py
@@ -0,0 +1,11 @@
+import imp
+from operator import imod
+from django.urls import path
+
+
+from .views import IndexView, AboutView
+
+urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
+    path('about/', AboutView.as_view(), name='about'),
+]
